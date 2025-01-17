d5b19d28179844b528cf32a65109508248685deb
GhostMasterSc <tolunayalankaya@gmail.com>
2025-01-17 17:41:01 +0300

    Update favicon and fix react-helmet-async dependency

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,25 @@ import Home from './pages/Home';
 import Product from './pages/Product';
 import Solutions from './pages/Solutions';
 import About from './pages/About';
+import { HelmetProvider, Helmet } from 'react-helmet-async';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="product" element={<Product />} />
-          <Route path="solutions" element={<Solutions />} />
-          <Route path="about" element={<About />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <HelmetProvider>
+      <Helmet>
+        <title>Wolfgang Consultancy</title>
+      </Helmet>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="product" element={<Product />} />
+            <Route path="solutions" element={<Solutions />} />
+            <Route path="about" element={<About />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </HelmetProvider>
   );
 }
 
